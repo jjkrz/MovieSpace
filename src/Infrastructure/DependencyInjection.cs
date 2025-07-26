87bdfe2aa187e880538bf685b0796431539e49d5
jjkrz <jj.krzewski@gmail.com>
2025-07-26 14:20:51 +0200

    project init

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(connectionString));
+
+            return services;
+        }
+    }
+}
---
+++ src/WebApi/Program.cs
@@ -0,0 +1,38 @@
+using Application;
+using Infrastructure;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+// Add services to the container.
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(configuration);
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
