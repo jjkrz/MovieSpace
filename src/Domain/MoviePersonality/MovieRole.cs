5c54554da9b94b0fd83d6dc1cde263c4a3bc91be
jjkrz <jj.krzewski@gmail.com>
2025-08-14 21:48:01 +0200

    Remove unnecessary constructors in domain entities

--- src/Domain/MoviePersonality/MovieRole.cs
+++ src/Domain/MoviePersonality/MovieRole.cs
@@ -4,10 +4,6 @@ namespace Domain.MoviePersonality
 {
     public sealed class MovieRole: Entity
     { 
-        public MovieRole(Guid id): base(id)
-        {
-        }
-
         public string RoleName { get; private set; } = null!;
         public string? CharacterName { get; private set; }
     }
