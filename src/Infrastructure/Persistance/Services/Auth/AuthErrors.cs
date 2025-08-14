f076487f84ff4e694133e55e275b20fe1dc89ae2
jjkrz <jj.krzewski@gmail.com>
2025-08-14 21:45:23 +0200

    Add user context service

--- src/Infrastructure/Persistance/Services/Auth/AuthErrors.cs
+++ src/Infrastructure/Persistance/Services/Auth/AuthErrors.cs
@@ -7,6 +7,5 @@ namespace Infrastructure.Persistance.Services.AuthService
         public static Error RegistrationFailed(string messages) => new Error(ErrorType.Validation, messages);
         public static Error UserNotFound => new Error(ErrorType.NotFound, "User was not found");
         public static Error InvalidCredentials => new Error(ErrorType.Unauthorized, "Invalid credentials");
-        public static Error TokenGenerationFailed(string message) => new Error(ErrorType.Unexpected, message);
     }
 }
---
+++ src/Infrastructure/Persistance/Services/Jwt/Errors/JwtErrors.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Infrastructure.Persistance.Services.Jwt.Errors
+{
+    public static class JwtErrors
+    {
+        public static Error TokenGenerationFailed(string message) => new Error(ErrorType.Unexpected, message);
+        public static Error IdFetchingFailed => new Error(ErrorType.NullValue, "Could not fetch UserId");
+
+    }
+}
