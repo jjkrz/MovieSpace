85ecaf46dbdd1595d5aa0190646912c7bf2e5f96
jjkrz <Jakub@DESKTOP-7579T38>
2025-08-16 13:30:41 +0200

    Add paginated movie search functionality

---
+++ src/Application/Movies/GetMovies/GetMoviesQuery.cs
@@ -0,0 +1,9 @@
+﻿using Application.Abstractions;
+using Application.Helpers;
+
+namespace Application.Movies.GetMovies
+{
+    public record GetMoviesQuery(int Page, int PageSize): IQuery<PaginatedResponse<MovieBriefDto>>
+    {
+    }
+}
---
+++ src/Application/Movies/GetMovies/MovieBriefDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Movies.GetMovies
+{
+    public class MovieBriefDto
+    {
+        public string Title { get; set; } = null!;
+        public string Genres { get; set; } = null!;
+        public DateOnly ReleaseDate { get; set; }
+        public int AverageRating { get; set; }
+    }
+}
