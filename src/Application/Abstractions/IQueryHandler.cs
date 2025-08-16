b5e6478374cd06ecb8dfc20b6abe2852503f893c
jjkrz <Jakub@DESKTOP-7579T38>
2025-08-16 13:28:11 +0200

    Add query handler abstraction

---
+++ src/Application/Abstractions/IQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using MediatR;
+
+namespace Application.Abstractions
+{
+    public interface IQueryHandler<TQuery> : IRequestHandler<TQuery, Result>
+        where TQuery : IQuery
+    {
+    }
+
+    public interface IQueryHandler<TQuery, TResult> : IRequestHandler<TQuery, Result<TResult>>
+        where TQuery : IQuery<TResult>
+    {
+    }
+}
