6155c3a179f2a792d6d8cc888e00a9cbd55ea866
jjkrz <Jakub@DESKTOP-7579T38>
2025-08-16 13:28:36 +0200

    Add paginated response helpers

---
+++ src/Application/Helpers/PageInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.Helpers
+{
+    public class PageInfo
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public long TotalElements { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasNext { get; set; }
+        public bool HasPrevious { get; set; }
+
+        public PageInfo(int currentPage, int pageSize, long totalElements)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            TotalElements = totalElements;
+            TotalPages = (int)Math.Ceiling((double)totalElements / pageSize);
+            HasNext = currentPage < TotalPages - 1;
+            HasPrevious = currentPage > 0;
+        }
+    }
+}
