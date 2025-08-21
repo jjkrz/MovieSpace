b714124010c7d8a3a17f7a4b380485894ef1b632
jjkrz <jj.krzewski@gmail.com>
2025-08-21 10:49:06 +0200

    Add movie person repository

--- src/Infrastructure/Persistance/ApplicationDbContext.cs
+++ src/Infrastructure/Persistance/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Domain.Common;
 using Domain.Movies;
+using Domain.MoviePeople;
+using Domain.MoviePersonality;
 using Infrastructure.Persistance.Identity;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
@@ -13,6 +15,9 @@ namespace Infrastructure.Database
         public DbSet<Movie> Movies { get; private set; }
         public DbSet<Genre> Genres { get; private set; }
         public DbSet<ProductionCountry> ProductionCountries { get; private set; }
+        public DbSet<MoviePerson> MoviePeople { get; private set; }
+        public DbSet<MovieRole> MovieRoles { get; private set; }
+        public DbSet<MoviePersonRole> MoviePersonRoles { get; private set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
