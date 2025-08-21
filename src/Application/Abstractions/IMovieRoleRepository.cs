07d9487fc1a1d95afc41003b00d4145a0a455223
jjkrz <jj.krzewski@gmail.com>
2025-08-21 13:24:33 +0200

    Add movie role repository

---
+++ src/Application/Abstractions/IMovieRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.MoviePersonality;
+
+namespace Application.Abstractions
+{
+    public interface IMovieRoleRepository
+    {
+        Task<bool> ExistsWithName(string RoleName, CancellationToken cancellationToken = default);
+        Task CreateAsync(MovieRole movieRole, CancellationToken cancellationToken = default);
+        Task<MovieRole?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        void Delete(MovieRole role);
+        Task<ICollection<MovieRole>> GetPaginatedAsync(int page, int size, string? search = null, CancellationToken cancellationToken = default);
+        Task<int> GetCountAsync(CancellationToken cancellationToken = default);
+    }
+}
